53c45ab4fedc6cb571d5f032fe0006c217ea2e74
john-martens <john.martens@gmail.com>
2020-09-15 12:09:27 -0400

    Worked on Pizza 1

--- src/PizzaCost1.java
+++ src/PizzaCost1.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class PizzaCost1 {
 
     public static void main(String[] args) {
-       
+       Scanner s = new Scanner(System.in);
+       final double labour = 1.5;
+       final double heat = 0.75;
     }
     
 }
