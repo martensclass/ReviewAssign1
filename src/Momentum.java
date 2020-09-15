c94925c24731ee27ba8f2c1df5cc8014d89bf5cd
john-martens <john.martens@gmail.com>
2020-09-15 11:41:38 -0400

    Initial commit

---
+++ src/Momentum.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Momentum {
+
+    public static void main(String[] args) {
+
+      double momentum, mass, velocity;
+      
+      Scanner s = new Scanner(System.in);
+      
+      //ask for mass and velocity
+      System.out.print("Enter object's mass (in kg) >> ");
+      mass = s.nextDouble();
+      System.out.print("Enter object's velocity (in m/second) >> ");
+      velocity = s.nextDouble();
+      
+      //calculate momentum
+      momentum = mass * velocity;
+      
+      //print the answer
+      System.out.println(String.format("The object's momentum is %.2f", momentum));
+      
+        
+    }
+    
+}
---
+++ src/PizzaCost1.java
@@ -0,0 +1,7 @@
+public class PizzaCost1 {
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
